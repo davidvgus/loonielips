91f96d887bece09b6e2483e53e05460d02e96f0f
David Gustafson <git@qqrtfm.com>
2021-11-16 13:12:23 -0800

    first

---
+++ LoonyLips.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _ready():
+	var prompts = ["Yann", "banana", "tingly", "glam"]
+	var story : String = "name %s noun %s adj %s adj %s "
+	print( prompts)
+
+	$VBoxContainer/DisplayText.text = (story % prompts)
+
+
+func _on_LineEdit_text_entered(new_text):
+	update_DisplayText(new_text)
+
+func update_DisplayText(new_text):
+	$VBoxContainer/DisplayText.text = new_text
+	$VBoxContainer/LineEdit.clear()
